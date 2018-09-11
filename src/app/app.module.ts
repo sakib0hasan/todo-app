efe695d27dd678b7f42076ba49bd451f460bafaa
Sakib Hasan <ajobbalok22@gmail.com>
2018-09-12 00:41:47 +0600

    basic todo done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
+import {AppComponent} from './app.component';
+import {TodoListComponent} from './components/todo-list/todo-list.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TodoListComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatCheckboxModule,
+        MatToolbarModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        FormsModule,
+        MatButtonModule,
+        MatIconModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Todo} from '../../model/todo';
+import {FormControl} from '@angular/forms';
+import * as _ from 'lodash';
 
 @Component({
     selector: 'app-todo-list',
@@ -9,14 +11,33 @@ import {Todo} from '../../model/todo';
 export class TodoListComponent implements OnInit {
 
     todos = [];
+    randomThings = [
+        'Install Google Analytics.',
+        'Using WordPress? Download the Yoast SEO Plugin.',
+        'Verify Your Site in Google Search Console.',
+        'Improve Website Performance & Speed.',
+        'Take the Google Mobile-Friendly Test',
+        'Develop a Buyer Personal',
+        'Keyword Strategy',
+        'Brainstorm for Keyword Ideas.',
+        'Narrow Down Your Keyword List',
+        'Choose Target Keywords'
+    ];
+    taskname = new FormControl('');
 
     constructor() {
     }
 
     ngOnInit() {
-        for(let i = 1; i < 10; i++){
-            let t = new Todo(i, "task "+i, false);
+        for (let i = 0; i < 10; i++) {
+            let t = new Todo(i, this.randomThings[i], false);
             this.todos.push(t);
         }
+    }
+
+    onTaskComplete(id, isChecked) {
+        _.find(this.todos, todo => {
+            return todo.id === id;
+        }).completed = isChecked;
     }
 }
